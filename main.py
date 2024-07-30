97ed86ab80111d0e98d1913f69e8864e5a27a502
Mehrzad30 <mehrzadhosseinizadeh@gmail.com>
2024-07-30 12:49:03 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+time =int(input("slam Sara sat chanse"))
+if time< 6 and time> 0:
+    print("name shab bekheyr!")
+if time< 12 and time> 6:
+    print("sobh bekheyr!")
+if time< 18 and time> 12:
+    print("badazohr bekheyr!")
+if time< 24 and time> 18:
+    print("shab bekheyr!")
+
+
